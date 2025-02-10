53ff9c7db73ef7bca49e002e208b6a41499c433b
Juyounge-e <ghn09081@gmail.com>
2025-02-11 03:32:59 +0900

    ✨ : slack api 조회하는 함수

---
+++ src/slack/slack_api.py
@@ -0,0 +1,30 @@
+import requests
+
+# acces_token 조회 api(아직 개발 안됨)
+SLACK_TOKEN_API = "https://www.branchify.site/api/oauth/slack/token"
+
+def get_slack_access_token(user_email: str, assistant_name: str):
+    params = {
+        "userEmail": user_email,
+        "assistantName": assistant_name
+    }
+
+    response = requests.get(SLACK_TOKEN_API, params=params)
+
+    if response.status_code == 200:
+        slack_data = response.json()
+        slack_token = slack_data.get("access_token")
+        if slack_token:
+            print(f"✅ Slack Access Token 가져오기:: {slack_token[:10]}...")
+            return slack_token
+        else:
+            raise ValueError("❌ Slack Access Token이 응답에 포함되지 않음")
+    else:
+        raise Exception(f"❌ 오류 발생! 응답 코드: {response.status_code}\n응답 내용: {response.text}")
+
+# 테스트
+if __name__ == "__main__":
+    TEST_EMAIL = "test@gmail.com"
+    TEST_ASSISTANT_NAME = "assistant1"
+    token = get_slack_access_token(TEST_EMAIL, TEST_ASSISTANT_NAME)
+    print(f"🔑 Slack Token: {token[:10]}...")
