b9d77914d33a5472333dc6293d865f52d134436d
Juyounge-e <ghn09081@gmail.com>
2025-02-13 01:54:18 +0900

    ✨ : 워크스페이스 봇 초대 기능 추가

--- src/slack/slack_setup.py
+++ src/slack/slack_setup.py
@@ -1,12 +1,15 @@
+import os
+import sys
 import requests
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 from src.slack.slack_api import get_slack_access_token
 
-# 태스트 정보
-user_email = "test@gmail.com"
-assistant_name = "assistant1"
+# 사용자 입력할때마다 조회하는 걸로 변경 
+USER_EMAIL = "test@gmail.com"
+ASSISTANT_NAME = "assistant1"
 
-# 백엔드 API를 통해 Access Token 조회 
-SLACK_BOT_TOKEN = get_slack_access_token(user_email, assistant_name)
+SLACK_BOT_TOKEN = get_slack_access_token(USER_EMAIL, ASSISTANT_NAME)
 
 def get_bot_user_id():
     url = "https://slack.com/api/auth.test"
@@ -20,13 +23,31 @@ def get_bot_user_id():
 
     if data.get("ok"):
         bot_user_id = data.get("user_id")
-        print(f"✅ Slack Bot User ID: {bot_user_id}")
+        print(f" Slack Bot User ID: {bot_user_id}")
         return bot_user_id
     else:
-        raise Exception(f"❌ Slack Bot User ID 가져오기 실패 응답: {data}")
+        raise Exception(f" Slack Bot User ID 가져오기 실패 응답: {data}")
 
 
-# 채널에 봇 추가 함수 
+def check_bot_in_workspace():
+    url = "https://slack.com/api/users.list"
+    headers = {
+        "Authorization": f"Bearer {SLACK_BOT_TOKEN}",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    if data.get("ok"):
+        bot_members = [user for user in data.get("members", []) if user.get("is_bot")]
+        bot_names = [bot.get("name") for bot in bot_members]
+        print(f"워크스페이스에 추가된 봇 목록: {bot_names}")
+        return bot_names
+    else:
+        raise Exception(f"워크스페이스 내 봇 조회 실패 응답: {data}")
+
+
 def invite_bot_to_channel(channel_id):
     bot_user_id = get_bot_user_id()
 
@@ -44,11 +65,15 @@ def invite_bot_to_channel(channel_id):
     response_data = response.json()
 
     if response.status_code == 200 and response_data.get("ok"):
-        print(f"✅ Slack Bot이 채널 {channel_id}에 추가")
+        print(f"Slack Bot이 채널 {channel_id}에 추가되었습니다.")
     else:
-        print(f"❌ Bot 채널 초대 실패 응답: {response_data}")
+        print(f"Bot 채널 초대 실패 응답: {response_data}")
+
 
 
 if __name__ == "__main__":
-    SLACK_CHANNEL_ID = ""  # 실제 채널 id로 테스트 
-    invite_bot_to_channel(SLACK_CHANNEL_ID)
+    print("🚀 Slack Setup 스크립트 실행 중...")
+
+    bot_names = check_bot_in_workspace()
+    
+    print("Slack Setup 완료!")
